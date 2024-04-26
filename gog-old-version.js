6db17384926be64aa1f1c42508233ff827ea3b64
Willow0349 <113538504+Willow0349@users.noreply.github.com>
2024-04-26 14:20:07 +0200

    Create gog-old-version.js

---
+++ gog-old-version.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Force old website version for gog.com
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Always use the old version of the gog.com website
+// @author       Willow0349 (Github: https://github.com/Willow0349)
+// @match        https://*.gog.com/*
+// @exclude      https://*.gog.com/*checkout*
+// @exclude      https://*.gog.com/forum/*
+// @icon         https://store-static-modular.gog-statics.com/en/assets/favicons/apple-touch-icon-180x180.png
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Set the cookie
+    document.cookie = "front_ab=old; path=/";
+  
+    }
+})();
